d5095947faa083be6a586cc2ab4822ea1bc27030
Sahil Koditkar <46161593+sahilkoditkar@users.noreply.github.com>
2022-05-09 19:33:50 +0530

    use curl instead of wget

--- get_terraform.sh
+++ get_terraform.sh
@@ -1,3 +1,3 @@
-wget https://releases.hashicorp.com/terraform/1.1.9/terraform_1.1.9_linux_amd64.zip
+curl https://releases.hashicorp.com/terraform/1.1.9/terraform_1.1.9_linux_amd64.zip --output terraform_1.1.9_linux_amd64.zip
 unzip terraform_1.1.9_linux_amd64.zip
 rm terraform_1.1.9_linux_amd64.zip
